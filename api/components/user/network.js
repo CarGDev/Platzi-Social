5a29145532c3c885b368ae7568380d6be1c99f8d
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-08-31 23:41:26 -0500

    Adding documentation for the api

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,12 +1,45 @@
 const express = require('express')
 
 const response = require('../../../network/response')
-const controller = require('./controller')
+const Controller = require('./index')
 
 const router = express.Router()
-router.get('/', function (req, res) {
-  const lista = controller.list()
-  response.success(req, res, lista, 200)
-})
+
+// Routes
+router.get('/', list)
+router.get('/:id', get)
+router.post('/', upsert)
+router.put('/', upsert)
+
+// Internal Functions
+function list(req, res) {
+  Controller.list()
+    .then((lista) => {
+      response.success(req, res, lista, 200)
+    })
+    .catch((err) => {
+      response.error(req, res, err.message, 500)
+    })
+}
+
+function get(req, res) {
+  Controller.get(req.params.id)
+    .then((user) => {
+      response.success(req, res, user, 200)
+    })
+    .catch((err) => {
+      response.error(req, res, err.message, 500)
+    })
+}
+
+function upsert(req, res) {
+  Controller.upsert(req.body)
+    .then((user) => {
+      response.success(req, res, user, 201)
+    })
+    .catch((err) => {
+      response.error(req, res, err.message, 500)
+    })
+}
 
 module.exports = router
