3e94e0a75a48fac1758d5301c5bd2fd34dd743f8
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-08-31 22:47:07 -0500

    Creating the db file

---
+++ network/response.js
@@ -0,0 +1,19 @@
+exports.success = function (req, res, message, status) {
+  const statusCode = status || 200
+  const statusMessage = message || ''
+  res.status(status).send({
+    error: false,
+    status: statusCode,
+    body: statusMessage
+  })
+}
+
+exports.error = function (req, res, message, status) {
+  const statusCode = status || 500
+  const statusMessage = message || 'Internal server error'
+  res.status(statusCode).send({
+    error: false,
+    status: statusCode,
+    body: statusMessage
+  })
+}
