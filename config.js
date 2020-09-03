a1cbf176322cce37e6910a7030b7e03fb8844f1e
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-09-02 23:15:30 -0500

    Adding secrets to now

--- config.js
+++ config.js
@@ -4,17 +4,17 @@ module.exports = {
     port: process.env.API_PORT || 3000
   },
   jwt: {
-    secret: process.env.JWT_SECRET || 'notasecret!'
+    secret: process.env.JWT_SECRET || ''
   },
   mysql: {
-    host: process.env.MYSQL_HOST || 'remotemysql.com',
-    user: process.env.MYSQL_USER || 'BbIt13PEOS',
-    password: process.env.MYSQL_PASS || '2otZxJO3ot',
-    database: process.env.MYSQL_DB || 'BbIt13PEOS'
+    host: process.env.MYSQL_HOST || '',
+    user: process.env.MYSQL_USER || '',
+    password: process.env.MYSQL_PASS || '',
+    database: process.env.MYSQL_DB || ''
   },
   mysqlService: {
     port: process.env.MYSQL_SRV_PORT || 3001,
-    host: process.env.MYSQL_SRV_HOST || 'localhost'
+    host: process.env.MYSQL_SRV_HOST || ''
   },
   post: {
     port: process.env.POST_PORT || 3002
